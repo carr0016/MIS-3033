63b3265d3347595903b585e326f2371b1fb0eba2
Riley Carr <rileycarr@ou.edu>
2021-01-31 18:25:38 -0600

    student practice

---
+++ 3033-Review/Student/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = (File.ReadAllLines("rileystudentDataRandom100Rows.csv"));
+
+            List<Student> students = new List<Student>();
+
+
+            Student stud = new Student();
+            stud.FirstName = "riley";
+            stud.LastName = "Carr";
+            stud.FavoriteColor = "blue";
+            stud.ID = 1;
+            Console.WriteLine(stud.CreateFullName());
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                string pieces = line.Split(',');
+
+                int id = Convert.ToInt32(pieces[0]);
+
+                Student currentStudent = new Student()
+
+
+
+            }
+
+            ///foreach (var item in lines)
+            ///{
+           ///     Console.WriteLine(line);
+          ///  }
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
---
+++ 3033-Review/Student/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set;}
+
+        public Student()
+        {
+            ID = -900;
+            FirstName = "";
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+
+        public Student(int id, string first, string last, string color)
+        {
+            ID = id;
+            FirstName = first;
+            LastName = last;
+            FavoriteColor = color;
+        }
+        public Student(int id)
+        {
+            ID = id;
+            FirstName = "";
+            LastName = string.Empty;
+            FavoriteColor = string.Empty;
+        }
+        public string CreateFullName()
+        {
+            //same as return LastName + ", " + FirstName;
+            return $"{LastName}, {FirstName}";
+
+        }
+
+
+    }
+
+
+
+}
