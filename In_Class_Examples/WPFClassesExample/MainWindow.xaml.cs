d5d5e16c0dcfa912c9ba35145f196cb50a58ad48
Riley Carr <rileycarr@ou.edu>
2021-02-08 11:54:55 -0600

    in class class example

---
+++ In_Class_Examples/WPFClassesExample/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFClassesExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name, breed, url;
+
+            name = txtName.Text;
+            if (string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("You must enter a name!");
+                return;
+            }
+            breed = txtBreed.Text;
+            url = txtURL.Text;
+
+            Pet myPet = new Pet();
+            {
+                Breed = breed,
+                Name = name,
+                PicURL = url;
+            };
+
+            1stPets.Items.Add(myPet);
+
+
+
+        }
+    }
+}
---
+++ In_Class_Examples/WPFClassesExample/Pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPFClassesExample
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string PicURL { get; set; }
+
+        public Pet()
+        {
+            Name = string.Empty;
+            Breed = string.Empty;
+            PicURL = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Breed})";
+        }
+    }
+}
