3176770115ff78dfb5f9522547a462c978d941f7
Riley Carr <rileycarr@ou.edu>
2021-02-22 12:31:28 -0600

    didn't finish on creating new window

--- In_Class_Examples/WPF-JSON_RickAndMorty/MainWindow.xaml.cs
+++ In_Class_Examples/WPF-JSON_RickAndMorty/MainWindow.xaml.cs
@@ -44,5 +44,12 @@ namespace WPF_JSON_RickAndMorty
                
             }
         }
+
+        private void lstCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Character c = (Character)lstCharacters
+            CharacterInfoWindow infoWindow = new CharacterInfoWindow();
+            infoWindow.URL = "";
+        }
     }
 }
