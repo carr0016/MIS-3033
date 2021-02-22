2d5595fec80d7833a4b2128209dd6d1885fdbd22
Riley Carr <rileycarr@ou.edu>
2021-02-22 11:57:18 -0600

    created rick and morty participation

---
+++ In_Class_Examples/WPF-JSON_RickAndMorty/RickAndMortyAPI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_JSON_RickAndMorty
+{
+    public class RickAndMortyAPI
+    {
+
+        ///https://rickandmortyapi.com/api/character
+        public Info info {get;set;}
+
+         
+    }
+
+    public class Info //used lightbulb and clicked generate class
+    {
+
+    }
+}
