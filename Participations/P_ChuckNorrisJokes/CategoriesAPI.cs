470ef00fe8d87c7ef4b8f8789d970ab63396478c
Riley Carr <rileycarr@ou.edu>
2021-03-08 16:30:24 -0600

    added properties

---
+++ Participations/P_ChuckNorrisJokes/CategoriesAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_ChuckNorrisJokes
+{
+    public class CategoriesAPI
+    {
+        public int created_at { get; set; }
+        public string icon_url { get; set; }
+        public int id { get; set; }
+        public int updated_at { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+    }
+}
