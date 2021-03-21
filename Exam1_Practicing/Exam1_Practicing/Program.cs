a22f40483dc5cccb3483e18700766a8543b5aefe
Riley Carr <rileycarr@ou.edu>
2021-03-21 17:53:12 -0500

    practicing for test

---
+++ Exam1_Practicing/Exam1_Practicing/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exam1_Practicing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Dictionary<int, List<Receipt>> receipts = new Dictionary<int, List<Receipt>>();
+            string answer;
+
+
+            do
+            {
+
+                int cogs = ValidateIntegerInput("Enter the quantity of cogs >>", "Sorry that it is invalid. Please try again.");
+                int gears = ValidateIntegerInput("Enter the quantity of gears >>", "Sorry that it is invalid. Please try again.");
+                int id = ValidateIntegerInput("Enter the customer's ID >>", "Sorry that it is invalid. Please try again.");
+
+                Receipt receipt = new Receipt(id, cogs, gears);
+
+                if (receipts.ContainsKey(id) == false)
+                {
+                    receipts.Add(id, new List<Receipt>());
+                }
+
+                receipts[id].Add(receipt);
+
+                Console.WriteLine("Do you want to enter information for another receipt? (Yes,No)");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
+
+            do
+            {
+                DisplayOptions();
+                answer = Console.ReadLine();
+                int choice = 0;
+                while (int.TryParse(answer, out choice) == false | (choice != 3 && choice != 2 && choice != 1))
+                {
+                    Console.WriteLine($"(answer) was not a valid input. Please look at the menu and try again");
+                    DisplayOptions();
+                    answer = Console.ReadLine();
+                }
+                switch (choice)
+                {
+                    case 1:
+                        int id = ValidateIntegerInput("Please enter the customer id that you want to see receipts from >>", "Sorry that was not a valid ID. Enter an actual customer ID")
+                        DisplayReceiptsByCustomerID(receipts, id);
+                        break;
+                    case 2:
+                        DisplayReceiptsForToday(receipts);
+                        break;
+
+                    case 3:
+                        DisplayReceiptsWithHighestTotal(receipts);
+                        break;
+                }
+
+
+                Console.WriteLine("Do you want to display more reciepts? (Yes or No)");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
+
+            Console.WriteLine("Bye");
+        }
+
+        private static void DisplayReceiptsWithHighestTotal(Dictionary<int, List<Receipt>> receipts)
+        {
+            double highest = -1;
+            foreach (var customer in receipts.Keys)
+            {
+                foreach (var receipt in receipts[customer])
+                {
+                    if (receipt.CalculateTotal() > highest)
+                    {
+                        highest = receipt.CalculateTotal();
+                    }
+                }
+            }
+            foreach (var customer in receipts.Keys)
+            {
+                foreach (var receipt in receipts[customer])
+                {
+                    if (receipt.CalculateTotal() == highest)
+                    {
+                        receipt.PrintReceipt();
+                    }
+                }
+            }
+        }
+
+        private static void DisplayReceiptsForToday(Dictionary<int, List<Receipt>> receipts)
+        {
+            foreach (var customer in receipts.Keys)
+            {
+                foreach (var receipt in receipts[customer])
+                {
+                    if (receipt.SaleDate.Date == DateTime.Now.Date)
+                    {
+                        receipt.PrintReceipt();
+                    }
+                }
+            }
+        }
+
+        private static void DisplayReceiptsByCustomerID(Dictionary<int, List<Receipt>> receipts, int id)
+        {
+            foreach (var receipt in receipts[id])
+            {
+                receipt.PrintReceipt();
+            }
+        }
+
+        private static void DisplayOptions()
+        {
+            Console.WriteLine("".PadRight(15, '$'));
+            Console.WriteLine("\tOptions");
+            Console.WriteLine("1. By Customer ID");
+            Console.WriteLine("2. All of todays Receipt's");
+            Console.WriteLine("3. Receipt with highest total");
+            Console.WriteLine("".PadRight(15, '#'));
+            Console.WriteLine();
+        }
+
+        private static int ValidateIntegerInput(string initialMessage, string repromptMessage)
+        {
+            int value;
+            string input;
+            Console.WriteLine(initialMessage);
+            input = Console.ReadLine();
+            while (int.TryParse(input, out value) == false)
+            {
+                Console.WriteLine(repromptMessage);
+                input = Console.ReadLine();
+            }
+
+            return value;
+
+        }
+    }
+}
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Exam1_Practicing/Exam1_Practicing/Receipt.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Review_HW1
+{
+    public class Receipt
+    {
+        public int CustomerID { get; set; }
+        public int CogQuantity { get; set; }
+        public int GearQuantity { get; set; }
+        public DateTime SaleDate { get; set; }
+        private double SalesTaxPercent { get; set; }
+        private double CogPrice { get; set; }
+        private double GearPrice { get; set; }
+
+
+
+        public Receipt()
+        {
+            CustomerID = 0;
+            CogQuantity = 0;
+            GearQuantity = 0;
+            SaleDate = DateTime.Now;
+            SalesTaxPercent = .089;
+            CogPrice = 79.99;
+            GearPrice = 250.00;
+        }
+
+        public Receipt(int id, int cog,int gear)
+        {
+            CustomerID = id;
+            CogQuantity = cog;
+            GearQuantity = gear;
+        }
+
+        public double CalculateTotal()
+        {
+            double netAmount = CalculateNetAmount();
+            double taxAmount = CalculateTaxAmount();
+
+            return netAmount + taxAmount;
+        }
+
+        public void PrintReceipt()
+        {
+            string receipt = 
+                      $"\t{string.Empty.PadLeft(40, '#')}\n" +
+                      $"\t{"".PadLeft(05, ' ') + "Customer:"} {CustomerID}\n" +
+                      $"\t{string.Empty.PadLeft(40, '-')}\n" +
+                      $"\t{"# of Cogs:".PadRight(15, ' ')}{CogQuantity.ToString("N0")}\\N" +
+                      $"\t{"# of Gears:".PadRight(15, ' ')}{GearQuantity.ToString("N0")}\\N" +
+                      $"\t{"Subtotal:".PadRight(15, ' ')}{CalculateNetAmount().ToString("C")}\\N" +
+                      $"\t{"Sales Tax:".PadRight(15, ' ')}{CalculateTaxAmount().ToString("C")}\\N" +
+                      $"\t{"Total:".PadRight(15, ' ')}{CalculateTotal().ToString("C")}\\N" +
+                      $"\t{string.Empty.PadLeft(40, '#')}";
+            //$"{"Customer:"} {CustomerID}" +
+            //$"{"Number of COGS:"} {CogQuantity}" +
+            //$"{"Number of Gears:"} {GearQuantity}" +
+            //$"{"Sub Total:"} {CalculateNetAmount()}" +
+            //$"{"Sales Tax:"} {CalculateTaxAmount()}" +
+            //$"{Total:"} {CaclulateTotal()}";
+
+
+        }
+
+        private double CalculateTaxAmount()
+        {
+                return CalculateNetAmount() * SalesTaxPercent;
+
+        }
+
+        private double CalculateNetAmount()
+        {
+            double netAmount, cogpricewithMarkup, gearpricewithMarkup;
+            if (CogQuantity>10 | GearQuantity>10 | (CogQuantity+GearQuantity) >16)
+            {
+                cogpricewithMarkup = CogPrice * .125;
+                gearpricewithMarkup = GearPrice * .125;
+
+            }
+            else
+            {
+                cogpricewithMarkup = CogPrice * .15;
+                gearpricewithMarkup = GearPrice * .15;
+            }
+            netAmount = CogQuantity * cogpricewithMarkup + GearQuantity * gearpricewithMarkup;
+            return netAmount;
+        }
+    }
+
+}
