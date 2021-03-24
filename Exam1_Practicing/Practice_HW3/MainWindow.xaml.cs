e86674daeae0e89f4690782334d02b537441493b
Riley Carr <rileycarr@ou.edu>
2021-03-23 19:19:40 -0500

    finished practice problems

---
+++ Exam1_Practicing/Practice_HW3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_HW3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<TVShow> TVShows = new List<TVShow>; 
+        public MainWindow()
+        {
+            InitializeComponent();
+            var lines = File.ReadAllLines("TV Show Data.txt");
+        }
+
+        private void cboRating_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboCountry_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cboLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Exam1_Practicing/Practice_HW3/TVShow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_HW3
+{
+    class TVShow
+    {
+        public string Actors { get; set; }
+        public string Awards { get; set; }
+        public string Country { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+        public string Rated { get; set; }
+        public string Released { get; set; }
+        public string RuntimeInMinutes { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Writer { get; set; }
+        public string Year { get; set; }
+        public string ImdbID { get; set; }
+        public string ImdbRating { get; set; }
+        public string ImdbVotes { get; set; }
+        public string TotalSeasons { get; set; }
+
+        public TVShow ()
+        {
+            Actors = String.Empty;
+            Awards = String.Empty;
+            Country = String.Empty;
+            Director = String.Empty;
+            Genre = String.Empty;
+            Language = String.Empty;
+            Plot = String.Empty;
+            Poster = String.Empty;
+            Rated = String.Empty;
+            Released = String.Empty;
+            RuntimeInMinutes = String.Empty;
+            Title = String.Empty;
+            Type = String.Empty;
+            Writer = String.Empty;
+            Year = String.Empty;
+            ImdbID = String.Empty;
+            ImdbRating = String.Empty;
+            ImdbVotes = String.Empty;
+            TotalSeasons = String.Empty;
+        }
+
+    }
+}
