20d3e0a56e6e00f7f520221d6564f1350e29e20a
Riley Carr <rileycarr@ou.edu>
2021-04-07 12:14:00 -0500

    in class practice

---
+++ In_Class_Examples/MVC_Intro/Controllers/MIS3033.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Intro.Controllers
+{
+    public class MIS3033 : Controller
+    {
+        public IActionResult Index()
+        {
+            Student s = new Student()
+            {
+                FirstName = "Miach",
+                LastName = "Tison"
+            };
+
+            return View();
+        }
+    }
+}
